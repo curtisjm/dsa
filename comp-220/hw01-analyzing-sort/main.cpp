e693cda9c25e8a51f62b8336d8a991f0e849003e
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-08-26 18:35:24 -0700

    add sort

--- comp-220/hw01-analyzing-sort/main.cpp
+++ comp-220/hw01-analyzing-sort/main.cpp
@@ -9,21 +9,24 @@
 
   https://docs.google.com/spreadsheets/d/1gGLdji0SbLiGeoUvvcCdixevRXZ5XWg2hDedQvOxumA/edit?usp=sharing 
 
-
 */
 
-using namespace std; // select as standard library for main (CAVEAT: never use this in .h files or large projects!)
+using namespace std;
 
-static long swapCount = 0; // used to keep track of swaps
-static long compareCount = 0; // used to keep track of compares
-static chrono::duration<double> time_span; // used to keep track of elapsed time
+// used to keep track of swaps
+static long swapCount = 0;
+// used to keep track of compares
+static long compareCount = 0;
+// used to keep track of elapsed time
+static chrono::duration<double> time_span;
 
 // return -1, 0, or 1 to indicate if a is < ==, or > b
 int compare(int a, int b) {
 	compareCount++;
 	if (a > b) return 1;
 	if (a < b) return -1;
-	return 0;  // they must be equal
+	// they must be equal
+	return 0;
 }
 
 // swap i and j and increment counter
@@ -34,21 +37,12 @@ void swap(int a[], int i, int j) {
 	a[j] = temp;
 }
 
-//
-// PRINT Values
-//
-
 void printValues(const char* msg, int a[], int N) {
 	cout << msg << endl;
 	for (int i = 0; i < N; i++)
 		cout << a[i] << ", ";
 	cout << endl;
 }
-
-
-//
-// PRINT STATS
-//
 
 void printStats(int N) {
 	cout << "\nPrint Stats" << endl;
@@ -65,61 +59,97 @@ void printStats(int N) {
 	swapCount = 0;
 }
 
+int partition(int arr[], int left, int right) {
+	int pivot = arr[right];
+	// i is the index of the smaller number
+	int i = left - 1;
 
-//
-// mySort() 
-//
+	// j keeps track of current number that we are examining
+	for (int j = left; j <= right - 1; j++) {
+		if (compare(arr[j], pivot) == -1) {
+			i++;
+			swap(arr, i, j);
+		}
+		// all nums up to i are < pivot
+		// all nums up to j are >= pivot
+	}
 
-void mySort(int a[], int N) {
-
-	chrono::steady_clock::time_point start_time = chrono::steady_clock::now(); // start time
-
-	// TODO Write your sort function here
-
-
-	chrono::steady_clock::time_point end_time = chrono::steady_clock::now(); // end time
-
-	time_span = chrono::duration_cast<chrono::duration<double>>(end_time - start_time); // duration
+	// make sure pivot is in between two groups
+	swap(arr, i + 1, right);
+	// return index of the pivot
+	return (i + 1);
 }
 
-//
-// isSortedAscending() - called after sorting to verify results
-//
+void quickSort(int arr[], int left, int right) {
+	// already in right place
+	if (left >= right) {
+		return;
+	}
 
+	// beginning partition index
+	int p = partition(arr, left, right);
+
+	// left / lower side of array
+	quickSort(arr, left, p - 1);
+	// right / upper side of array
+	quickSort(arr, p + 1, right);
+}
+
+void mySort(int a[], int N) {
+	// start time
+	chrono::steady_clock::time_point start_time = chrono::steady_clock::now();
+
+	// pass array, starting index, and ending index
+	quickSort(a, 0, N - 1);
+
+	// end time
+	chrono::steady_clock::time_point end_time = chrono::steady_clock::now();
+
+	// duration
+	time_span = chrono::duration_cast<chrono::duration<double>>(end_time - start_time);
+}
+
+// called after sorting to verify results
 bool isSortedAscending(int a[], int N) {
-
 	for (int i = 0; i < N - 1; i++) {
-
-		if (a[i] > a[i + 1]) // the order of these 2 are inverted, so the array is not ascending
+		// the order of these 2 are inverted, so the array is not ascending
+		if (a[i] > a[i + 1])
 			return false;
 	}
 
-	return true; // if we got this afar, the array must be sorted in ascending order
+	// if we got this afar, the array must be sorted in ascending order
+	return true;
 }
 
 int main() {
 	std::cout << "HW My Sort\n";
 
-	int N = 64; // number of items we are sorting
-	int a[N]; // array of N items
+	// number of items we are sorting
+	const int N = 64;
+	// array of N items
+	int a[N];
 
 	for (int i = 0; i < N; i++) {
-		a[i] = rand() % N + 1; // generate random values in the range of 1 to N, inclusive
+		// generate random values in the range of 1 to N, inclusive
+		a[i] = rand() % N + 1;
 	}
 
-	if (N < 100) printValues("Initial Values: ", a, N); // only print if N is small, to avoid clogging up the console
+	// only print if N is small, to avoid clogging up the console
+	if (N < 100) printValues("Initial Values: ", a, N);
 
-	mySort(a, N); // sort array a containing N values (TODO - you need to write this!)
+	// sort array a containing N values (TODO - you need to write this!)
+	mySort(a, N);
 
-	if (N < 100) printValues("\nSorted Values: ", a, N); // only print if N is small, to avoid clogging up the console
+	// only print if N is small, to avoid clogging up the console
+	if (N < 100) printValues("\nSorted Values: ", a, N);
 
-	printStats(N); // print out the statistics for mySort() in terms of compares and swaps
+	// print out the statistics for mySort() in terms of compares and swaps
+	printStats(N);
 
-	if (!isSortedAscending(a, N)) { // did the sort work?  this is an error check to be sure your sort worked ok
+	// did the sort work? this is an error check to be sure your sort worked ok
+	if (!isSortedAscending(a, N)) {
 		cout << "ERROR: mySort() failed - the array is not in ascending order" << endl;
 	}
 
-
 	cout << "DONE" << endl;
-
 }
